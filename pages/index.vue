9cbfe2c66cf7ee526f1c407d31c735af384d90c6
Michał Maciejewski <maciejewskimichal7@gmail.com>
2023-09-22 16:08:46 +0200

    feat: :sparkles: initial commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+
+const { data } = useFetch('/api/hello')
+
+</script>
+
+<template>
+  <div>
+    Page: index
+    {{ data }}
+  </div>
+</template>
+
+<style scoped></style>
